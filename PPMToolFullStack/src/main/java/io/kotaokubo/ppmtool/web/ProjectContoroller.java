fb527886129726902131ad036cefdb54763f0603
kotaokubo <kutpremium@gmail.com>
2019-11-24 20:01:33 +0900

    create: errorservicefile

--- PPMToolFullStack/src/main/java/io/kotaokubo/ppmtool/web/ProjectContoroller.java
+++ PPMToolFullStack/src/main/java/io/kotaokubo/ppmtool/web/ProjectContoroller.java
@@ -2,6 +2,7 @@ package io.kotaokubo.ppmtool.web;
 
 
 import io.kotaokubo.ppmtool.domain.Project;
+import io.kotaokubo.ppmtool.services.MapValidationErrorService;
 import io.kotaokubo.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -25,20 +26,15 @@ public class ProjectContoroller {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
 
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
 
-        if(result.hasErrors()) {
-
-            Map<String, String> errorMap = new HashMap<>();
-
-            for(FieldError error: result.getFieldErrors()) {
-                errorMap.put(error.getField(), error.getDefaultMessage());
-            }
-
-            return new ResponseEntity<Map<String, String >>(errorMap, HttpStatus.BAD_REQUEST);
-        }
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if(errorMap != null) return errorMap;
 
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
