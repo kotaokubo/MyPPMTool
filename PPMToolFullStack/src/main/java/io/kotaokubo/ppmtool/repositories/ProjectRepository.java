919650bf9c5ecb3ff6ebe2d8e9c7b9f166958dd3
kotaokubo <kutpremium@gmail.com>
2019-11-24 16:21:01 +0900

    add repository change Project

---
+++ PPMToolFullStack/src/main/java/io/kotaokubo/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,12 @@
+package io.kotaokubo.ppmtool.repositories;
+
+import io.kotaokubo.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
