f1087530cd24759da5781ebfc6ad9ea95ecd8ea7
kotaokubo <kutpremium@gmail.com>
2019-12-01 11:20:55 +0900

    import: bootstrap

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Dashboard />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Dashboard />
+      </div>
+    );
+  }
 }
 
 export default App;
