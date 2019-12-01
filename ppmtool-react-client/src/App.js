7249cf743b6a2d9dbeaa0ebd55a267c90d90fbc3
kotaokubo <kutpremium@gmail.com>
2019-12-01 10:24:11 +0900

    create: Project file and Layout file

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
+import Header from "./components/Layout/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Dashboard />
     </div>
   );
