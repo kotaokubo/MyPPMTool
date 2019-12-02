5dcb2b7afe94d8f9c07840683e5d7353c5071e2a
kotaokubo <kutpremium@gmail.com>
2019-12-02 15:25:35 +0900

    add: react-router

---
+++ ppmtool-react-client/src/components/Project/AddProject.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class AddProject extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Add Project</h1>
+      </div>
+    );
+  }
+}
+
+export default AddProject;
