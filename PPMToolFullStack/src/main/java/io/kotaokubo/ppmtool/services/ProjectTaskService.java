28441ca062922d016a7bca81b33e860a0a7a61d1
kotaokubo <kutpremium@gmail.com>
2019-12-17 20:29:55 +0900

    post method

---
+++ PPMToolFullStack/src/main/java/io/kotaokubo/ppmtool/services/ProjectTaskService.java
@@ -0,0 +1,54 @@
+package io.kotaokubo.ppmtool.services;
+
+import io.kotaokubo.ppmtool.domain.Backlog;
+import io.kotaokubo.ppmtool.domain.Project;
+import io.kotaokubo.ppmtool.domain.ProjectTask;
+import io.kotaokubo.ppmtool.exceptions.ProjectIdException;
+import io.kotaokubo.ppmtool.repositories.BacklogRepository;
+import io.kotaokubo.ppmtool.repositories.ProjectTaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectTaskService {
+
+    @Autowired
+    private BacklogRepository backlogRepository;
+
+    @Autowired
+    private ProjectTaskRepository projectTaskRepository;
+
+
+    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
+
+        //Exceptions: Project not found
+
+        //PTs to be added to a specific project, project != null, BL exists
+        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+        //set the bl to pt
+        projectTask.setBacklog(backlog);
+        //we want our project sequence to be like this: IDPRO-1  IDPRO-2  ...100 101
+        Integer BacklogSequence = backlog.getPTSequence();
+        // Update the BL SEQUENCE
+        BacklogSequence++;
+
+        backlog.setPTSequence(BacklogSequence);
+
+        //Add Sequence to Project Task
+        projectTask.setProjectSequence(projectIdentifier+"-"+BacklogSequence);
+        projectTask.setProjectIdentifier(projectIdentifier);
+
+        //INITIAL priority when priority null
+//        if(projectTask.getPriority()==0||projectTask.getPriority()==null){
+//            projectTask.setPriority(3);
+//        }
+        //INITIAL status when status is null
+        if(projectTask.getStatus() == null) {
+            projectTask.setStatus("TO_DO");
+        }
+
+        return projectTaskRepository.save(projectTask);
+    }
+
+
+}
---
+++ PPMToolFullStack/src/main/java/io/kotaokubo/ppmtool/web/BacklogController.java
@@ -0,0 +1,35 @@
+package io.kotaokubo.ppmtool.web;
+
+import io.kotaokubo.ppmtool.domain.ProjectTask;
+import io.kotaokubo.ppmtool.services.MapValidationErrorService;
+import io.kotaokubo.ppmtool.services.ProjectTaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/backlog")
+@CrossOrigin
+public class BacklogController {
+
+    @Autowired
+    private ProjectTaskService projectTaskService;
+
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
+    @PostMapping("/{backlog_id}")
+    public ResponseEntity<?> addPTtoBacklog(@Valid @RequestBody ProjectTask projectTask, BindingResult result, @PathVariable String backlog_id) {
+
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if(errorMap != null) return errorMap;
+
+        ProjectTask projectTask1 = projectTaskService.addProjectTask(backlog_id, projectTask);
+
+        return new ResponseEntity<ProjectTask>(projectTask1, HttpStatus.CREATED);
+    }
+}
