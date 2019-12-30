f5cca73cd21e6810739d8f8a3a6cfa25e0c09140
kotaokubo <kutpremium@gmail.com>
2019-12-30 17:03:49 +0900

    BUX FIX: delete method cascade

--- PPMToolFullStack/src/main/java/io/kotaokubo/ppmtool/domain/Backlog.java
+++ PPMToolFullStack/src/main/java/io/kotaokubo/ppmtool/domain/Backlog.java
@@ -22,7 +22,7 @@ public class Backlog {
     private Project project;
 
     //OneToMany projectTasks
-    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "backlog")
+    @OneToMany(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER, mappedBy = "backlog", orphanRemoval = true)
     private List<ProjectTask> projectTasks = new ArrayList<>();
 
 
