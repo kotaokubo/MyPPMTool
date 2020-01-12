76f84cf643b3fd9af5b7c6e1802fe026f8f6b375
kotaokubo <kutpremium@gmail.com>
2020-01-12 19:26:13 +0900

    project task delete

--- ppmtool-react-client/src/reducers/backlogReducer.js
+++ ppmtool-react-client/src/reducers/backlogReducer.js
@@ -23,8 +23,10 @@ export default function(state = initialState, action) {
       };
     case DELETE_PROJECT_TASK:
       return {
-        ...state
-        //Todo
+        ...state,
+        project_tasks: state.project_tasks.filter(
+          project_task => project_task.projectSequence !== action.payload
+        )
       };
     default:
       return state;
