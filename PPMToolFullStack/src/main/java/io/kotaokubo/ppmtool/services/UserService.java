a919c7ee3565fb8ba401b1b55f3c861b82fc56a6
kotaokubo <kutpremium@gmail.com>
2020-01-13 20:37:21 +0900

    POST: user

--- PPMToolFullStack/src/main/java/io/kotaokubo/ppmtool/services/UserService.java
+++ PPMToolFullStack/src/main/java/io/kotaokubo/ppmtool/services/UserService.java
@@ -1,7 +1,9 @@
 package io.kotaokubo.ppmtool.services;
 
+import io.kotaokubo.ppmtool.domain.User;
 import io.kotaokubo.ppmtool.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,4 +11,13 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public User saveUser(User newUser) {
+        newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+
+        return userRepository.save(newUser);
+    }
 }
--- PPMToolFullStack/src/main/java/io/kotaokubo/ppmtool/web/UserController.java
+++ PPMToolFullStack/src/main/java/io/kotaokubo/ppmtool/web/UserController.java
@@ -1,4 +1,39 @@
 package io.kotaokubo.ppmtool.web;
 
+import io.kotaokubo.ppmtool.domain.User;
+import io.kotaokubo.ppmtool.services.MapValidationErrorService;
+import io.kotaokubo.ppmtool.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result) {
+
+        // valid passwords match
+
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if(errorMap != null) return errorMap;
+
+        User newUser = userService.saveUser(user);
+
+        return new ResponseEntity<User>(newUser, HttpStatus.CREATED);
+    }
 }
