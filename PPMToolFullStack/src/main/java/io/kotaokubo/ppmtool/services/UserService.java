86f4837c587ca83ce05350b9e0c7397bc4578ded
kotaokubo <kutpremium@gmail.com>
2020-01-13 17:06:52 +0900

    create: user repository & service

---
+++ PPMToolFullStack/src/main/java/io/kotaokubo/ppmtool/services/UserService.java
@@ -0,0 +1,12 @@
+package io.kotaokubo.ppmtool.services;
+
+import io.kotaokubo.ppmtool.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+}
