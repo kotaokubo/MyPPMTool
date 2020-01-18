e88bcb73fff4cd70a1ca439c5f1a7a156b41dd94
kotaokubo <kutpremium@gmail.com>
2020-01-18 17:52:20 +0900

    token generate

--- PPMToolFullStack/src/main/java/io/kotaokubo/ppmtool/security/SecurityContants.java
+++ PPMToolFullStack/src/main/java/io/kotaokubo/ppmtool/security/SecurityContants.java
@@ -4,7 +4,7 @@ public class SecurityContants {
 
     public static final String SIGN_UP_URLS = "/api/users/**";
     public static final String H2_URL = "h2-console/**";
-    public static final String SECRET = "SecretkeyToGenJWTs";
+    public static final String SECRET = "SecretKeyToGenJWTs";
     public static final String TOKEN_PREFIX = "Bearer";
     public static final String HEADERSTRING = "Authrization";
     public static final long EXPIRATION_TIME = 30_000; //30secontd
--- PPMToolFullStack/src/main/java/io/kotaokubo/ppmtool/web/UserController.java
+++ PPMToolFullStack/src/main/java/io/kotaokubo/ppmtool/web/UserController.java
@@ -1,19 +1,25 @@
 package io.kotaokubo.ppmtool.web;
 
 import io.kotaokubo.ppmtool.domain.User;
+import io.kotaokubo.ppmtool.payload.JWTLoginSuccessResponse;
+import io.kotaokubo.ppmtool.payload.LoginRequest;
+import io.kotaokubo.ppmtool.security.JwtTokenProvider;
 import io.kotaokubo.ppmtool.services.MapValidationErrorService;
 import io.kotaokubo.ppmtool.services.UserService;
 import io.kotaokubo.ppmtool.validator.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+
+import static io.kotaokubo.ppmtool.security.SecurityContants.TOKEN_PREFIX;
 
 @RestController
 @RequestMapping("/api/users")
@@ -27,6 +33,29 @@ public class UserController {
 
     @Autowired
     private UserValidator userValidator;
+
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest, BindingResult result) {
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if (errorMap != null) return errorMap;
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = TOKEN_PREFIX + tokenProvider.generateToken(authentication);
+
+        return ResponseEntity.ok(new JWTLoginSuccessResponse(true, jwt));
+    }
 
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result) {
