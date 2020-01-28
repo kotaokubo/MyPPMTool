935ac3970e8ac20549247f08d8ade0391aa1b21b
kotaokubo <kutpremium@gmail.com>
2020-01-28 16:01:55 +0900

    projectTask user specific read and create

--- PPMToolFullStack/src/main/java/io/kotaokubo/ppmtool/web/BacklogController.java
+++ PPMToolFullStack/src/main/java/io/kotaokubo/ppmtool/web/BacklogController.java
@@ -10,6 +10,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -24,20 +25,20 @@ public class BacklogController {
     private MapValidationErrorService mapValidationErrorService;
 
     @PostMapping("/{backlog_id}")
-    public ResponseEntity<?> addPTtoBacklog(@Valid @RequestBody ProjectTask projectTask, BindingResult result, @PathVariable String backlog_id) {
+    public ResponseEntity<?> addPTtoBacklog(@Valid @RequestBody ProjectTask projectTask, BindingResult result, @PathVariable String backlog_id, Principal principal) {
 
         ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
         if(errorMap != null) return errorMap;
 
-        ProjectTask projectTask1 = projectTaskService.addProjectTask(backlog_id, projectTask);
+        ProjectTask projectTask1 = projectTaskService.addProjectTask(backlog_id, projectTask, principal.getName());
 
         return new ResponseEntity<ProjectTask>(projectTask1, HttpStatus.CREATED);
     }
 
     @GetMapping("/{backlog_id}")
-    public Iterable<ProjectTask> getProjectBacklog(@PathVariable String backlog_id) {
+    public Iterable<ProjectTask> getProjectBacklog(@PathVariable String backlog_id, Principal principal) {
 
-        return projectTaskService.findBacklogById(backlog_id);
+        return projectTaskService.findBacklogById(backlog_id, principal.getName());
     }
 
     @GetMapping("/{backlog_id}/{pt_id}")
@@ -49,7 +50,7 @@ public class BacklogController {
     }
 
     @PatchMapping("/{backlog_id}/{pt_id}")
-    public ResponseEntity<?> updateProjectTask(@PathVariable String backlog_id, @PathVariable String pt_id, @Valid @RequestBody ProjectTask projectTask, BindingResult result) {
+    public ResponseEntity<?> updateProjectTask(@PathVariable String backlog_id, @PathVariable String pt_id, @Valid @RequestBody ProjectTask projectTask, BindingResult result, Principal principal) {
 
         ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
         if (errorMap != null) return errorMap;
