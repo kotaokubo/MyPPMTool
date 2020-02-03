508ff8d148b3d130530dff22dff67119a2ccbb93
kotaokubo <kutpremium@gmail.com>
2020-02-03 23:25:17 +0900

    security action and reducer

---
+++ ppmtool-react-client/src/reducers/securityReducer.js
@@ -0,0 +1,27 @@
+import { SET_CURRENT_USER } from "../actions/types";
+
+const initialState = {
+  validToken: false,
+  user: {}
+};
+
+const booleanActionPayload = payload => {
+  if (payload) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case SET_CURRENT_USER:
+      return {
+        ...state,
+        calidToken: booleanActionPayload(action.payload),
+        user: action.payload
+      };
+    default:
+      return state;
+  }
+}
